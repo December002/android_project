f195e0fa3bd2ece8f1eb8216ed379a538c16dd0f
Parkjiyong <freeman7572@naver.com>
2023-08-10 12:40:34 +0900

    게임 운석 추가, 맥주병 게임 추가

--- step08gameview/src/main/java/com/example/step08gameview/GameActivity.kt
+++ step08gameview/src/main/java/com/example/step08gameview/GameActivity.kt
@@ -1,7 +1,9 @@
 package com.example.step08gameview
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 
 class GameActivity : AppCompatActivity() {
     //사운드 메니저 객체
@@ -15,14 +17,30 @@ class GameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // xml 문서를 전개해서 화면 구성하기
+        setContentView(R.layout.activity_game)
         //GameView 객체를 생성해서 참조값을 변수에 담기
-        val gameView=GameView(this)
-        //GameView 로 화면을 체우기
-        setContentView(gameView)
+        val gameView=findViewById<GameView>(R.id.gameView)
+
         //SoundManager 객체를 생성해서 필드에 저장하기
         sManager = SoundManager(this)
         //GameView 의 필드에 SoundManager 객체 넣어주기
         gameView.sManager=sManager
+        //GameOver 버튼의 참조값을 얻어와서
+        var overBtn=findViewById<Button>(R.id.gameOverBtn)
+        //버튼을 보이지 않게 하고
+        overBtn.isVisible=false
+        //GameView 에 버튼의 참조값을 넣어주고
+        gameView.overBtn=overBtn
+        //버튼에 리스너 등록
+        overBtn.setOnClickListener{
+            //버튼을 누르면 액티비티가 종료되게 한다.
+            finish() //액티비티의 finish() 메소드를 호출하면 액티비티가 종료된다.
+        }
+        //Intent 객체에 담겨진 boolean type Extra 얻어내기
+        val isMute=intent.getBooleanExtra("isMute", false)
+        //SoundManager 객체에 isMute 값을 넣어 준다.
+        sManager.isMute=isMute
     }
 
     override fun onStart() {
